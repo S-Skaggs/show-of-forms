3c694bc96f15d9ebfe61a566fea4999fa99f3df3
S-Skaggs <s-skaggs@outlook.com>
2024-12-03 16:45:33 -0500

    BUG: Removed a trailing comma.

--- src/app/my-reactive-form/my-reactive-form.component.ts
+++ src/app/my-reactive-form/my-reactive-form.component.ts
@@ -85,7 +85,7 @@ export class MyReactiveFormComponent implements OnInit {
     name: [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(100)])],
     description: ['', Validators.compose([Validators.maxLength(500)])],
     quantity: [0, Validators.compose([Validators.required, Validators.min(0)])],
-    price: [0.01, Validators.compose([Validators.required, Validators.min(0.01), Validators.pattern('/^\d*\.?\d?\d?$/')])],
+    price: [0, Validators.compose([Validators.required, Validators.min(0.01), Validators.pattern('/^\d*\.?\d?\d?$/')])]
   });
 
   constructor(private fb: FormBuilder) {
